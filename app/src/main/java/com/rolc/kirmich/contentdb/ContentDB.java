58767685af5bb3f06a7f6e2f2bf845a32ba9c9cc
Aman Mangal <mangalaman93@gmail.com>
2016-03-24 13:40:27 -0400

    android-backend : add layout for database

---
+++ app/src/main/java/com/rolc/kirmich/contentdb/ContentDB.java
@@ -0,0 +1,12 @@
+package com.rolc.kirmich.contentdb;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.Set;
+
+interface ContentDB {
+    Set<String> getAllTags(Context context);
+    Cursor Search(String[] tags, int limit);
+    void getItem(int id);
+}
